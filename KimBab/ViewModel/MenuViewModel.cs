48723a12de6353a52f96680bff73d0c35471a198
Hyo-Seong <dlgytjd0907@naver.com>
2018-08-17 16:44:34 +0900

    add viewmodel

---
+++ KimBab/ViewModel/MenuViewModel.cs
@@ -0,0 +1,45 @@
+﻿using KimBab.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KimBab.ViewModel
+{
+    public class MenuViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Menu> Items { get; set; }
+
+        private bool IsDataLoaded { get; set; }
+
+        public MenuViewModel()
+        {
+            Items = new ObservableCollection<Menu>();
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void AddMenu(Menu menu)
+        {
+            ////Items.Add(menu);
+            //Items.Add(new Menu
+            //{
+            //    Name = "GI",
+            //    Price = 40000,
+            //    Type = 0
+            //});
+            //Items.Add(new Menu
+            //{
+            //    Name = "GI",
+            //    Price = 40000,
+            //    Type = 0
+            //});        
+}
+    }
+}
---
+++ KimBab/ViewModel/TableViewModel.cs
@@ -0,0 +1,39 @@
+﻿using KimBab.Model;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace KimBab.ViewModel
+{
+    public class TableViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Table> Items { get; set; }
+
+        private bool IsDataLoaded { get; set; }
+
+        public TableViewModel()
+        {
+            Items = new ObservableCollection<Table>();
+            LoadData();
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    
+
+        public void LoadData()
+        {
+            Items.Add(new Table(1));
+            Items.Add(new Table(2));
+            Items.Add(new Table(3));
+            Items.Add(new Table(4));
+            Items.Add(new Table(5));
+            Items.Add(new Table(6));
+            Items.Add(new Table(7));
+            Items.Add(new Table(8));
+            Items.Add(new Table(9));
+        }
+    }
+}
