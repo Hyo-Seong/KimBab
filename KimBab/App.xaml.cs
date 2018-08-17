ccaf754a7ef699bea92ad1013c95987c674db329
Hyo-Seong <dlgytjd0907@naver.com>
2018-08-17 16:43:52 +0900

    init

---
+++ KimBab/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using KimBab.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace KimBab
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static MenuViewModel menuViewModel = new MenuViewModel();
+        public static TableViewModel tableViewModel = new TableViewModel();
+    }
+}
---
+++ KimBab/View/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using KimBab.Model;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows;
+
+namespace KimBab
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataListView.ItemsSource = App.tableViewModel.Items;
+            
+            Debug.WriteLine(DataListView.ItemsSource);
+        }
+    }
+}
