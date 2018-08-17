16fbd2167a6856add7005173cf3ee2b6a3aefa86
Hyo-Seong <dlgytjd0907@naver.com>
2018-08-17 16:44:18 +0900

    add model

---
+++ KimBab/Model/Menu.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KimBab.Model
+{
+    public enum FoodType
+    {
+        KIMBAB,
+        NOODLE,
+        SIKSA,
+        BUNSIK,
+        DONGAS
+    }
+
+    public class Menu : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string name;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                NotifyPropertyChanged("Name");
+            }
+        }
+
+        private int price;
+        public int Price
+        {
+            get => price;
+            set
+            {
+                price = value;
+                NotifyPropertyChanged("Price");
+            }
+        }
+
+        private int orders;
+        public int Orders
+        {
+            get => orders;
+            set
+            {
+                orders = value;
+            }
+        }
+
+        private FoodType type;
+        public FoodType Type
+        {
+            get => type;
+            set
+            {
+                type = value;
+                NotifyPropertyChanged("Type");
+            }
+        }
+    }
+}
---
+++ KimBab/Model/Table.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace KimBab.Model
+{
+    public class Table : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private int tableNum;
+        public int TableNum
+        {
+            get => tableNum;
+            set
+            {
+                tableNum = value;
+                NotifyPropertyChanged("TableNum");
+            }
+        }
+
+        private List<Menu> menu;
+        public List<Menu> Menu
+        {
+            get => menu;
+            set
+            {
+                menu = value;
+                NotifyPropertyChanged("Menu");
+            }
+        }
+
+        private int totalPrice;
+
+        public int TotalPrice
+        {
+            get => totalPrice;
+            set
+            {
+                totalPrice = value;
+                NotifyPropertyChanged("TotalPrice");
+            }
+        }
+
+        public Table(int TableNum)
+        {
+            tableNum = TableNum;
+        }
+    }
+}
