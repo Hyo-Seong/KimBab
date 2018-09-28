cb50fe8284aecf006f119579641b3dc592a27ef3
Hyo-Seong <dlgytjd0907@naver.com>
2018-09-28 15:10:14 +0900

    statistics init

---
+++ KimBab/ViewModel/StatisticsViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace KimBab.ViewModel
+{
+    class StatisticsViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Menu> Items { get; set; }
+
+        private bool IsDataLoaded { get; set; }
+
+        public StatisticsViewModel()
+        {
+            Items = new ObservableCollection<Menu>();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
