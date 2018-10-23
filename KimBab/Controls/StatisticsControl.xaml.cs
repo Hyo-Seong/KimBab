cbd7c3649af58d7e8a2a4b55999cee52f36d4e80
Hyo-Seong <dlgytjd0907@naver.com>
2018-10-23 23:14:37 +0900

    카테고리별 판매량
    수량 파악 완료, 이제 총액 구해야됨,

--- KimBab/Controls/StatisticsControl.xaml.cs
+++ KimBab/Controls/StatisticsControl.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -10,7 +9,8 @@ namespace KimBab.Controls
     /// </summary>
     public partial class StatisticsControl : UserControl
     {
-        private List<KeyValuePair<string, int>> MyValue = new List<KeyValuePair<string, int>>();
+        private List<KeyValuePair<string, int>> CategoryChartList = new List<KeyValuePair<string, int>>();
+        private List<KeyValuePair<string, int>> MenuChartList = new List<KeyValuePair<string, int>>();
 
         public StatisticsControl()
         {
@@ -21,28 +21,72 @@ namespace KimBab.Controls
 
         public void UpdateChart()
         {
-            MyValue.Clear();
+            InitList();
+
             int i = 0;
+            int[] typeCountArray = new int[] { 0, 0, 0, 0, 0 };
             foreach (Model.Menu menu in App.menuViewModel.Items)
             {
-                MyValue.Add(new KeyValuePair<string, int>(menu.Name, menu.Orders));
-                Debug.WriteLine(MyValue[i++].Value.ToString());
-            }
+                MenuChartList.Add(new KeyValuePair<string, int>(menu.Name, menu.Orders));
+                switch (menu.Type.ToString())
+                {
+                    case "KIMBAB":
+                        typeCountArray[0] += menu.Orders;
+                        break;
 
-            CategoryChart.DataContext = MyValue;
-            CategoryChart.Visibility = Visibility.Visible;
+                    case "NOODLE":
+                        typeCountArray[1] += menu.Orders;
+                        break;
+
+                    case "SIKSA":
+                        typeCountArray[2] += menu.Orders;
+                        break;
+
+                    case "BUNSIK":
+                        typeCountArray[3] += menu.Orders;
+                        break;
+
+                    case "DONGAS":
+                        typeCountArray[4] += menu.Orders;
+                        break;
+                }
+            }
+            SetCategoryList(typeCountArray);
+
+            CategoryChart.DataContext = CategoryChartList;
+            MenuChart.DataContext = MenuChartList;
+        }
+
+        private void SetCategoryList(int[] array)
+        {
+            CategoryChartList.Add(new KeyValuePair<string, int>("KIMBAB", array[0]));
+            CategoryChartList.Add(new KeyValuePair<string, int>("NOODLE", array[1]));
+            CategoryChartList.Add(new KeyValuePair<string, int>("SIKSA", array[2]));
+            CategoryChartList.Add(new KeyValuePair<string, int>("BUNSIK", array[3]));
+            CategoryChartList.Add(new KeyValuePair<string, int>("DONGAS", array[4]));
+        }
+
+        private void InitList()
+        {
+            CategoryChartList.Clear();
+            MenuChartList.Clear();
         }
 
         private void ChangeChartBtn_Click(object sender, RoutedEventArgs e)
         {
+
             Button button = sender as Button;
             if (button.Content.Equals("카테고리별 판매량"))
             {
                 button.Content = "메뉴별 판매량";
+                CategoryChart.Visibility = Visibility.Hidden;
+                MenuChart.Visibility = Visibility.Visible;
             }
             else
             {
                 button.Content = "카테고리별 판매량";
+                CategoryChart.Visibility = Visibility.Visible;
+                MenuChart.Visibility = Visibility.Hidden;
             }
         }
     }
